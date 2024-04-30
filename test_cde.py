19c969f4ae0ded27f933445a5d8239fb0e2f1b26
mronci <112618063+mronci@users.noreply.github.com>
2024-04-30 16:24:38 +0200

    third version

--- test_cde.py
+++ test_cde.py
@@ -1,36 +1,13 @@
-# import pandas as pd
-# from pyspark.sql import SparkSession
-
-# if __name__ == "__main__":
-#     # Create a SparkSession
-#     spark = SparkSession.builder \
-#         .appName("HelloWorld") \
-#         .getOrCreate()
-
-#     # Create a DataFrame with a single column named "message"
-#     data = ["Hello", "World"]
-#     df = spark.createDataFrame(pd.DataFrame(data, columns=["message"]))
-
-#     # Print the DataFrame
-#     df.show()
-
-#     # Stop the SparkSession
-#     spark.stop()
-
 from pyspark.sql import SparkSession
 
-if __name__ == "__main__":
-    # Create a SparkSession
-    spark = SparkSession.builder \
-        .appName("HelloWorld") \
-        .getOrCreate()
-
-    # Create a DataFrame with a single column named "message"
-    data = ["Hello", "World"]
-    df = spark.createDataFrame(data, ["message"])
-
-    # Print the DataFrame
-    df.show()
-
-    # Stop the SparkSession
-    spark.stop()
+spark = SparkSession.builder.appName("demo").getOrCreate()
+df = spark.createDataFrame(
+    [
+        ("sue", 32),
+        ("li", 3),
+        ("bob", 75),
+        ("heo", 13),
+    ],
+    ["first_name", "age"],
+)
+df.show()
