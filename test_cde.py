b58362871af1ccbb8aae00a777819f2bc814605f
mronci <112618063+mronci@users.noreply.github.com>
2024-04-30 16:09:49 +0200

    removed pandas dependency

--- test_cde.py
+++ test_cde.py
@@ -1,4 +1,22 @@
-import pandas as pd
+# import pandas as pd
+# from pyspark.sql import SparkSession
+
+# if __name__ == "__main__":
+#     # Create a SparkSession
+#     spark = SparkSession.builder \
+#         .appName("HelloWorld") \
+#         .getOrCreate()
+
+#     # Create a DataFrame with a single column named "message"
+#     data = ["Hello", "World"]
+#     df = spark.createDataFrame(pd.DataFrame(data, columns=["message"]))
+
+#     # Print the DataFrame
+#     df.show()
+
+#     # Stop the SparkSession
+#     spark.stop()
+
 from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
@@ -9,7 +27,7 @@ if __name__ == "__main__":
 
     # Create a DataFrame with a single column named "message"
     data = ["Hello", "World"]
-    df = spark.createDataFrame(pd.DataFrame(data, columns=["message"]))
+    df = spark.createDataFrame(data, ["message"])
 
     # Print the DataFrame
     df.show()
