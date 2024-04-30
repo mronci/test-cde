eb91206e33fd82492f8c80bbf3ef5f7509b42085
mronci <112618063+mronci@users.noreply.github.com>
2024-04-30 16:02:59 +0200

    adds requirements and test file

---
+++ test_cde.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+    # Create a SparkSession
+    spark = SparkSession.builder \
+        .appName("HelloWorld") \
+        .getOrCreate()
+
+    # Create a DataFrame with a single column named "message"
+    data = ["Hello", "World"]
+    df = spark.createDataFrame(pd.DataFrame(data, columns=["message"]))
+
+    # Print the DataFrame
+    df.show()
+
+    # Stop the SparkSession
+    spark.stop()
